6238b7e4283c491fb7683c4e148c7714be7e6957
gamze <gamzekandara@gmail.com>
2023-11-23 18:41:24 -0500

    Adds Index.js

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// code your solution here
+function saturdayFun(activity = "roller-skate"){
+    return "This Saturday, I want to " + activity + "!";
+
+}
+saturdayFun("bathe my dog");
+
+function mondayWork(toDo = "go to the office"){
+    return "This Monday, I will " + toDo + ".";
+
+}
+mondayWork("work from home");
+
+let wrapAdjective = function(style="*") {
+    return function(adj="special") {
+      return "You are " + style + adj + style + "!"; 
+    }
+  }
+
+  console.log(wrapAdjective("a hard worker"("*")));
+  console.log(wrapAdjective("a dedicated programmer")("||"));
